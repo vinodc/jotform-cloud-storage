84bed55926320ad40c358d07f85d6a3dd2948b3a
Vinod Chandru <vinod.chandru@gmail.com>
2014-10-19 20:35:10 -0700

    Adding upload button.

--- widget/index.js
+++ widget/index.js
@@ -1,24 +1,36 @@
- var explorer = window.Kloudless.explorer({
-     // JotForm Kloudless App ID
-     app_id: "Hs_l0xUXMPC4nsmJ0oABuSvK3gTZWNLnV7F4InGfYqixT28G",
-     multiselect: JFCustomWidget.getQueryString('multiselect') === 'true',
-     link: true,
-     direct_link: true,
-     types: ['files'],
-     computer: false,
- });
-explorer.choose();
+var links = [];
+
+var explorer = window.Kloudless.explorer({
+    // JotForm Kloudless App ID
+    app_id: "Hs_l0xUXMPC4nsmJ0oABuSvK3gTZWNLnV7F4InGfYqixT28G",
+    multiselect: true /*JFCustomWidget.getQueryString('multiselect') === 'true'*/,
+    link: true,
+    direct_link: true,
+    types: ['files'],
+    computer: false,
+});
+
+explorer.on('success', function(files) {
+    $("#files").empty();
+    $(".files-desc").hide();
+    var links = files.map(function(f) {
+        $("#files").append('<li><a target="_blank" href="' + f.link + '">' +
+                           f.name + '</a></li>');
+        return f.link;
+    });
+    if (links && links.length > 1) {
+        $(".files-desc").show();
+    }
+});
+
+explorer.choosify(document.getElementById('upload'));
 
 JFCustomWidget.subscribe("ready", function(){
-    explorer.choose();
-    explorer.on('success', function(files) {
-        var links = files.map(function(f) { return f.link; });
-        JFCustomWidget.subscribe("submit", function(){
-            var msg = {
-                valid: true,
-                value: links.join(" "),
-            };
-            JFCustomWidget.sendSubmit(msg);
-        });
+    JFCustomWidget.subscribe("submit", function(){
+        var msg = {
+            valid: true,
+            value: links.join(" "),
+        };
+        JFCustomWidget.sendSubmit(msg);
     });
 });
