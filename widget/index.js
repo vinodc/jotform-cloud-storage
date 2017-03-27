31a56f021f22e98d59c241476c9f826fa866e802
Vinod Chandru <vinod.chandru@gmail.com>
2017-03-27 11:42:25 -0700

    Updating JotForm SDK usage. Adding additional configurability.

--- widget/index.js
+++ widget/index.js
@@ -1,59 +1,80 @@
-var links = [];
+var returnData = [];
 var clicked = false;
+var initialized = false;
 
 var kloudlessAppId = "Hs_l0xUXMPC4nsmJ0oABuSvK3gTZWNLnV7F4InGfYqixT28G";
 var kloudlessAccountId = 45245627;
 
-var explorer = window.Kloudless.explorer({
-    // Defaults to the JotForm Kloudless App ID
-    app_id: JFCustomWidget.getQueryString('app_id') || kloudlessAppId,
-    multiselect: JFCustomWidget.getQueryString('multiselect') !== 'false',
-    link: true,
-    direct_link: true,
-    types: ['files'],
-    computer: true,
-});
-
 function resize() {
-    JFCustomWidget.requestFrameResize({
-        width: 700,
-        height: 515
-    });
+  JFCustomWidget.requestFrameResize({
+    width: 700,
+    height: 515
+  });
 }
 
-explorer.on('success', function(files) {
+function init() {
+  if (initialized) return false;
+
+  var wOptions = JFCustomWidget.getWidgetSettings();
+  var getLink = wOptions['getLink'] !== 'false';
+
+  var explorer = window.Kloudless.explorer({
+    // Defaults to the JotForm Kloudless App ID
+    app_id: wOptions['appId'] || kloudlessAppId,
+    multiselect: wOptions['multiselect'] !== 'false',
+    link: getLink,
+    direct_link: getLink,
+    types: ['files'],
+    computer: wOptions['allowComputerUpload'],
+  });
+
+  explorer.on('success', function(files) {
     $("#files").empty();
     $(".files-desc").hide();
 
-    links = files.map(function(f) {
+    returnData = files.map(function(f) {
+      if (getLink) {
         var validity = "";
         if (f.account === kloudlessAccountId) {
-            validity = " (valid for 30 days)";
+          validity = " (valid for 30 days)";
         }
 
         $("#files").append('<li><a target="_blank" href="' + f.link + '">' +
                            f.name + '</a>' + validity + '</li>');
         return f.link;
+      }
+      else {
+        $("#files").append('<li>' + f.path + '</li>');
+        return f.path;
+      }
     });
-    if (links && links.length >= 1) {
-        $(".files-desc").show();
-    }
-    JFCustomWidget.sendData({value: links.join(" ")});
-});
 
-$("#upload").click(function() {
+    if (returnData && returnData.length >= 1) {
+      $(".files-desc").show();
+    }
+
+    JFCustomWidget.sendData({value: returnData.join(" ")});
+  });
+
+  $("#upload").click(function() {
     clicked = true;
     resize();
     explorer.choose();
-});
+  });
+
+  initialized = true;
+}
 
 JFCustomWidget.subscribe("ready", function(){
-    if (clicked) resize();
-    JFCustomWidget.subscribe("submit", function(){
-        var msg = {
-            valid: true,
-            value: links.join(" "),
-        };
-        JFCustomWidget.sendSubmit(msg);
-    });
+  init();
+
+  if (clicked) resize();
+
+  JFCustomWidget.subscribe("submit", function(){
+    var msg = {
+      valid: true,
+      value: returnData.join(" "),
+    };
+    JFCustomWidget.sendSubmit(msg);
+  });
 });
